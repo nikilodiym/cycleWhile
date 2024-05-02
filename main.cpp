86d0cc438dbae05a261d3479a0418854816b6df8
Nikita Krenov <nikitabaram@gmail.com>
2024-05-02 19:49:05 +0200

    In this code, we have a  do - while  loop that will keep asking the user to enter a number until the user enters 0. The sum of all the numbers entered by the user is calculated and displayed at the end. The  do - while  loop is useful when you want to execute the loop body at least once, regardless of the condition.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main() {
+	int number, sum = 0;
+	do {
+		cout << "Enter a number: ";
+		cin >> number;
+		sum += number;
+	} while (number != 0);
+
+	cout << "The sum is: " << sum << endl;
 
 	system("pause");
 	return 0;

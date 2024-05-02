dfbfbddece521d1c7a04b8c4a8f9f0f4d1222e68
Nikita Krenov <nikitabaram@gmail.com>
2024-05-02 20:32:34 +0200

    the arithmetic mean of all integers from 1 to 1000.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,13 @@
 using namespace std;
 
 int main() {
+	int a = 1, b = 1000, i=1;
+	while (a < b) {
+		a++;
+		i += a;
+	}
+	double average = static_cast<double>(i + 1) / a;
+	cout << "Arithmetic average of numbers from 1 to 1000: " << average << endl;
 
 	system("pause");
 	return 0;

7ba7909cf4ac190d99edae9525e6d92a415a640f
Nikita Krenov <nikitabaram@gmail.com>
2024-05-02 19:20:47 +0200

    when u write a number it displays all the numbers to it

--- main.cpp
+++ main.cpp
@@ -3,6 +3,15 @@
 using namespace std;
 
 int main() {
+	int number;
+	int i = 0;
+	cout << "Enter a number: ";
+	cin >> number;
+
+	while (i < number) {
+		cout << i+1 << "" << endl;
+		i++;
+	}
 
 	system("pause");
 	return 0;

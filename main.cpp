e70f80b4163f5527b25217d43b4a90f0a92a29b0
Nikita Krenov <nikitabaram@gmail.com>
2024-05-02 19:31:38 +0200

    Print all number in giver range

--- main.cpp
+++ main.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 int main() {
+	int firstNumber, secondNumber, distance, finallyNumber;
+	cout << "Enter the first number: ";
+	cin >> firstNumber;
+	cout << "Enter the second number: ";
+	cin >> secondNumber;
+
+	if (firstNumber > secondNumber) {
+		distance = firstNumber;
+		firstNumber = secondNumber;
+		secondNumber = distance;
+	}
+
+	cout << "All numbers from the function: ";
+	finallyNumber = firstNumber;
+	while (finallyNumber <= secondNumber) {
+		cout << finallyNumber << " ";
+		finallyNumber++;
+	}
 
 	system("pause");
 	return 0;

2bac53c2e5feb2cad93ac5bafacad665d66f04f7
Nikita Krenov <nikitabaram@gmail.com>
2024-05-02 20:25:38 +0200

    a program that asks for two integers x and y, then calculates and outputs the value of x to the power of y.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int main() {
-
+	int x, y, i=1;
+	cout << "Enter x: ";
+	cin >> x;
+	cout << "Enter y: ";
+	cin >> y;
+	while (y > 0) {
+		i = i * x;
+		y--;
+	}
+	cout << "Result: " << i << endl;
 	system("pause");
 	return 0;
 }

033befd2c3b38e15edab6d4d697e937a400a08ae
Nikita Krenov <nikitabaram@gmail.com>
2024-05-02 20:46:50 +0200

    a program that displays the multiplication table by k, where k is the option number. For example, for the 7th option:
    7 x 2 = 14;
    
    7 x 3 = 21.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int main() {
+	int k, i = 1;
+	cout << "Enter the number: ";
+	cin >> k;
 
+	cout << "Multiplication table for " << k << endl;
+
+	while (i <= 10) {
+		cout << k << " * " << i << " = " << (k * i) << endl;
+		i++;
+	}
 	system("pause");
 	return 0;
 }

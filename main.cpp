0430ecfb0d318dfed072b2bc7dce5dc25c126968
Nikita Krenov <nikitabaram@gmail.com>
2024-05-02 20:41:03 +0200

    the product of all integers from a to 20 (the value of a is entered from the keyboard: 1 <=a <= 20).

--- main.cpp
+++ main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
+	int a, sum = 0;
+	cout << "Enter a: ";
+	cin >> a;
+
+	do {
+		sum += a;
+		a++;
+	} while (1 <= a && a <= 20);
+
+	cout << "Sum: " << sum << endl;
 
 	system("pause");
 	return 0;

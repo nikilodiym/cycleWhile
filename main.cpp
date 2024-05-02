302d9067a434facd3b274d11c654acd566d2b300
Nikita Krenov <nikitabaram@gmail.com>
2024-05-02 20:17:53 +0200

    it is shown that the user enters a number and up to the number 500, the amount is output from his number, but there is one but, the amount is poorly output there, well, more precisely, it happens strangely

--- main.cpp
+++ main.cpp
@@ -3,6 +3,19 @@
 using namespace std;
 
 int main() {
+	int a, i, sum=0;
+	cout << "Enter a number: ";
+	cin >> a;
+
+	if (a <= 500) {
+		for (i = a; i <= 500; i++) {
+			sum += i;
+		}
+		cout << "Sum of integers from " << a << " to 500 " << sum << endl;
+	}
+	else {
+		cout << "The entered number a is greater than 500. Please enter a number less than or equal to 500" << endl;
+	}
 
 	system("pause");
 	return 0;

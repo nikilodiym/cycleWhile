d0e7151c83c95df213617a533bf601fc44b39433
Nikita Krenov <nikitabaram@gmail.com>
2024-05-02 19:46:37 +0200

    sum number in the range

--- main.cpp
+++ main.cpp
@@ -4,6 +4,29 @@ using namespace std;
 
 int main() {
 
+    int firstNumber, secondNumber;
+
+    cout << "Enter the start of the range: ";
+    cin >> firstNumber;
+    cout << "Enter the end of the range: ";
+    cin >> secondNumber;
+
+    if (firstNumber > secondNumber) {
+        int temp = firstNumber;
+        firstNumber = secondNumber;
+        secondNumber = temp;
+    }
+
+    int sum = 0;
+    int current = firstNumber;
+
+    while (current <= secondNumber) {
+        sum += current;
+        current++;
+    }
+
+	cout << "The sum of the numbers between " << firstNumber << " and " << secondNumber << " is " << sum << endl;
+
 	system("pause");
 	return 0;
 }
